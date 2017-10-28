66cdcfedc27b109bd86e940f29d1abc4f8eda3f7
vutuan95vn <anhtuan95vn@gmail.com>
2017-10-28 10:19:44 +0500

    day 1

---
+++ Zadanie_1/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Zadanie_1
+{
+    partial class CreateDBMSSQL
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(157, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Создать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnCreateDatabase);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Имя";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(279, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Подключиться";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnConnectDatabase);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(401, 54);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Создание или подключение к базам";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(13, 73);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(400, 85);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Добавление запись";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(278, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Добавить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // CreateDBMSSQL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 256);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "CreateDBMSSQL";
+            this.Text = "MS SQL mini";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Zadanie_1/Inquiries.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Zadanie_1
+{
+    class Inquiries
+    {
+        private SqlConnection myConn;
+        public void CreateDB(string nameDB)
+        {
+            String str;
+            SqlConnection mymewConn = new SqlConnection("Server=DESKTOP-906H1M0\\SQLEXPRESS;Integrated security=SSPI;database=master");
+
+            str = "CREATE DATABASE " + nameDB + " ON PRIMARY " +
+                "(NAME = MyDatabase_Data, " +
+                "FILENAME = 'C:\\Program Files\\Microsoft SQL Server\\MSSQL12.SQLEXPRESS\\MSSQL\\DATA\\" + nameDB + ".mfd', " +
+                "SIZE = 5MB, MAXSIZE = 20MB, FILEGROWTH = 10%) " +
+                "LOG ON (NAME = MyDatabase_Log, " +
+                "FILENAME = 'C:\\Program Files\\Microsoft SQL Server\\MSSQL12.SQLEXPRESS\\MSSQL\\DATA\\" + nameDB + ".ldf', " +
+                "SIZE = 5MB, " +
+                "MAXSIZE = 20MB, " +
+                "FILEGROWTH = 10%)";
+
+            SqlCommand myCommand = new SqlCommand(str, mymewConn);
+
+            try
+            {
+                mymewConn.Open();
+                myCommand.ExecuteNonQuery();
+                MessageBox.Show("DataBase is Created Successfully", "MyProgram", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "MyProgram", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            finally
+            {
+                if (mymewConn.State == ConnectionState.Open)
+                {
+                    mymewConn.Close();
+                }
+            }
+
+        }
+        public void CreateTable(string nameDB)
+        {
+            String str = "Server=DESKTOP-906H1M0\\SQLEXPRESS;Integrated security=SSPI;database=" + nameDB;
+            myConn = new SqlConnection(str);
+
+            str = "CREATE TABLE objejct (" +
+                    "	id INT PRIMARY KEY IDENTITY(1,1)," +
+                    "	[type] TEXT," +
+                    "	product TEXT )" +
+
+                  "CREATE TABLE attribute (" +
+                    "	id INT NOT NULL FOREIGN KEY REFERENCES objejct(id)," +
+                    "	name TEXT," +
+                    "	value TEXT)" +
+
+                  "CREATE TABLE connection (" +
+                    "	idparent INT NOT NULL FOREIGN KEY REFERENCES objejct(id)," +
+                    "	idchild INT NOT NULL FOREIGN KEY REFERENCES objejct(id)," +
+                    "	linkname TEXT)";
+
+            SqlCommand myCommand = new SqlCommand(str, myConn);
+
+            try
+            {
+                myConn.Open();
+                myCommand.ExecuteNonQuery();
+                MessageBox.Show("DataBase is Created Successfully", "MyProgram", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "MyProgram", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            finally
+            {
+                if (myConn.State == ConnectionState.Open)
+                {
+                    myConn.Close();
+                }
+            }
+
+        }
+
+        public void ConnectDB(string nameDB)
+        {
+            String str = "Server=DESKTOP-906H1M0\\SQLEXPRESS;Integrated security=SSPI;database=" + nameDB;
+            myConn = new SqlConnection(str);
+        }
+
+        public void AddDB(string nameDB)
+        {
+
+        }
+
+            //  Проверка подключение к базы //
+            /*try
+                {
+                    myConn.Open();
+                    myCommand.ExecuteNonQuery();
+                    MessageBox.Show("DataBase is Created Successfully", "MyProgram", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "MyProgram", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                finally
+                {
+                    if (myConn.State == ConnectionState.Open)
+                    {
+                        myConn.Close();
+                    }
+             }*/
+        }
+}
